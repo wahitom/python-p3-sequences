1cd5f7332eee8880f7eb578453f3938851ed7a1e
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-25 18:30:37 +0300

    Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+#fubonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0,1])
+    else:
+        fibonacci = [0, 1]
+        for i in range(2, length):
+            fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+
+        print(fibonacci)
+
+print_fibonacci(10)
